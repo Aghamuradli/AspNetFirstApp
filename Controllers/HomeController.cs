3021d76353ed2a859da5cce6279214714fa09e24
Babek Aghamuradov <82724876+BabekAga@users.noreply.github.com>
2022-02-20 20:46:59 +0400

    Asp

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ASP.NetFİrstApp.Controllers
+{
+
+    //action - her ansi bir seifenin ve ya her hansi bir mentiqin arxasinda duran FUNKSIYA
+    //bir kontroller icinde bir nece action ola biler 
+    //Her bir viewde ise bir action olur
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> students = new List<string>();
+            students.Add("Babek");
+            students.Add("Elshad");
+            students.Add("Aydin");
+
+
+            return View(students);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NetFİrstApp
+{
+    public class Startup
+    {
+        
+        public void ConfigureServices(IServiceCollection services) //Applicationa qosdugumuz modullar 
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) //--> //PIPELINE = MIDDLE WEAR(APPLICATIONA GEDEN YOL)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+               endpoints.MapControllerRoute(
+                   "default","{controller=Home}/{action=index}/{id?}"
+                   );
+            });
+        }
+    }
+}
